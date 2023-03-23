1122ebd923d6790cc9f1d81590f1396cd37c4ca6
Philipp Losbichler <philipp.losbichler@agravity.io>
2023-03-23 14:15:47 +0100

    Join publishAPI and generateAPI to one executable (generateAPI) - automatically creates SDK, restore & build project(s) and ask to publish on nuget;

--- generateAPI.ps1
+++ generateAPI.ps1
@@ -57,3 +57,28 @@ $csproj = Get-Content .\src\Agravity.Public\Agravity.Public.csproj
 $csproj = $csproj -replace "<Version>.*</Version>", "<Version>$apiVersion</Version>"
 $csproj | Set-Content .\src\Agravity.Public\Agravity.Public.csproj
 
+# echo apiVersion
+Write-Host "Build and Publish with apiVersion: $apiVersion"
+
+# build project with release
+dotnet build .\src\Agravity.Public\Agravity.Public.csproj -c Release
+
+# execute extract third party licenses
+.\extract_thirdparty_licenses.bat
+
+# build nuget package with version
+#create command 
+nuget pack -Build -OutputDirectory .\out .\src\Agravity.Public\Agravity.Public.csproj -Version $apiVersion
+
+# Set API Key (once)
+# nuget setApiKey xyz
+
+# prompt to publish
+Write-Host "Publish package in version $apiVersion? (y/n)"
+$publish = Read-Host
+
+# check if publish
+if ($publish -eq "y") {
+    # publish nuget package
+    dotnet nuget push .\out\Agravity.Public."$apiVersion".nupkg -s https://api.nuget.org/v3/index.json
+}

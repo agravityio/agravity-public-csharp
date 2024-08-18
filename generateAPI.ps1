8f512157eb6803028ad35ca3d7521c61b3d504ae
Philipp Losbichler <philipp.losbichler@agravity.io>
2024-08-18 23:41:27 +0200

    Add sleep to all replacements

--- generateAPI.ps1
+++ generateAPI.ps1
@@ -36,11 +36,18 @@ Set-Location .\src
 # replace all "Dictionary>" with "Dictionary<string, object>>"
 Get-ChildItem -Recurse -File | ForEach-Object { (Get-Content $_.FullName) -replace "Dictionary>", "Dictionary<string, object>>" | Set-Content $_.FullName }
 
+Start-Sleep -s 2
+
 # replace all "Dictionary<string, Object>" with "Dictionary<string, object>"
 Get-ChildItem -Recurse -File | ForEach-Object { (Get-Content $_.FullName) -replace "Dictionary<string, Object>", "Dictionary<string, object>" | Set-Content $_.FullName }
 
+Start-Sleep -s 2
+
 #replace all "Dictionary&gt;" with "Dictionary&lt;string, object&gt;&gt;"
 Get-ChildItem -Recurse -File | ForEach-Object { (Get-Content $_.FullName) -replace "Dictionary&gt;", "Dictionary&lt;string, object&gt;&gt;" | Set-Content $_.FullName }
+
+# wait 2 seconds
+Start-Sleep -s 2
 
 #replace all "StaticDefinedStaticDefinedList" with "StaticDefinedList"
 Get-ChildItem -Recurse -File | ForEach-Object { (Get-Content $_.FullName) -replace "StaticDefinedStaticDefinedList", "StaticDefinedList" | Set-Content $_.FullName }

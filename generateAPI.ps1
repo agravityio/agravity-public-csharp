fb7ecdd754f50ebd8e79d595d65fc969a78e728c
Philipp Losbichler <philipp.losbichler@agravity.io>
2024-09-02 23:15:03 +0200

    Add environment variables for API_KEY and OPENAPI_GENERATOR (so new version can be used) to generator

--- generateAPI.ps1
+++ generateAPI.ps1
@@ -1,7 +1,26 @@
+# check if $env:API_KEY is set, if not: exit
+if ($null -eq $env:AGRAVITY_API_KEY) {
+    Write-Host "Please set API_KEY environment variable"
+    # wait for user input
+    Write-Host "Press any key to continue ..."
+    exit
+}
+
+# check if $env:API_KEY is set, if not: exit
+if ($null -eq $env:OPENAPI_GENERATOR) {
+    Write-Host "Please set OPENAPI_GENERATOR path to the openapi-generator-cli.jar"
+    # wait for user input
+    Write-Host "Press any key to continue ..."
+    exit
+}
+
+
 # check REST API endpoint /version if backend is running, catch it and if it is not running: exit
-$version = (Invoke-RestMethod -Uri http://localhost:7072/api/version -Method Get -ContentType "application/json" -ErrorAction SilentlyContinue)
+$version = (Invoke-RestMethod -Uri http://localhost:7072/api/version  -Headers @{"x-functions-key" = $env:AGRAVITY_API_KEY} -Method Get -ContentType "application/json" -ErrorAction SilentlyContinue)
 if ($null -eq $version) {
     Write-Host "Please start backend before generating API"
+    # wait for user input
+    Write-Host "Press any key to continue ..."
     exit
 }
 
@@ -10,6 +29,8 @@ $apiVersion = $version.version
 # check if apiVersion is valid
 if ($null -eq $apiVersion) {
     Write-Host "Please provide apiVersion as parameter"
+    # wait for user input
+    Write-Host "Press any key to continue ..."
     exit
 }
 # echo apiVersion
@@ -26,10 +47,26 @@ Remove-Item -Path .\src -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "Calling public API"
 
+# download file with Authentication header (Bearer token)
+Invoke-WebRequest -Uri "http://localhost:7072/api/openapi/v3.json" -Headers @{"x-functions-key" = $env:AGRAVITY_API_KEY} -OutFile "openapi.json"
+
+# check if openapi.json exists and is not empty; if not: exit
+if (!(Test-Path "openapi.json") -or (Get-Content "openapi.json" -Raw) -eq "") {
+    Write-Host "openapi.json is empty or does not exist"
+    exit
+}
+
+# update @openapitools/openapi-generator-cli version
+#npm install @openapitools/openapi-generator-cli -g
+
 # generate API
-$params="packageName=Agravity.Public,library=restsharp,targetFramework=net6.0;netstandard2.0,packageVersion="+$apiVersion
+$params="packageName=Agravity.Public,library=restsharp,targetFramework=net8.0;netstandard2.0,packageVersion="+$apiVersion
 # Write-Host("Executing: openapi-generator generate -i https://api.agravity.com/v2/swagger.json -g csharp-netcore -o src -p "+$params);
-npx @openapitools/openapi-generator-cli generate -i http://localhost:7072/api/openapi/v3.json -g csharp-netcore -o . --additional-properties=$params
+#npx @openapitools/openapi-generator-cli generate -i openapi.json -g csharp-netcore -o . --additional-properties=$params
+java -jar $env:OPENAPI_GENERATOR generate -i openapi.json -g csharp -o . --additional-properties=$params
+
+# delete openapi.json
+Remove-Item -Path .\openapi.json -Force
 
 #change directory to src
 Set-Location .\src
